a39d002895d45b546e29d68da0f4eccefbaafbbc
Thiago Silveira <thiago1992@yahoo.com>
2022-02-09 20:25:23 -0700

    Initial Commit

---
+++ challenge/backend_program.rb
@@ -0,0 +1,11 @@
+# Delcaring 6 Variables for the backend program challenge
+
+book_name = "Harry Potter"
+
+book_discount = 0.10
+
+book_volume = 1
+
+book_bought = true
+
+books_on_sale = ['Harry Potter', 'Dune', 'Twilight']
---
+++ challenge/index.md
@@ -0,0 +1,7 @@
+# Book Titles
+
+1. The Alchemist
+2. Dune
+3. The Wind up Chronicles
+4. The Hobbit
+5. Harry Potter 1
